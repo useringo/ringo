d19513eac7527284f2642b7e8b80937d81252525
gmittal <gautam@mittal.net>
2015-05-22 15:04:56 -0700

    added stuff to server

--- server/app.js
+++ server/app.js
@@ -1,4 +1,7 @@
+// Copyright 2015 Gautam Mittal
+
 var bodyParser = require('body-parser');
+var fs = require('fs');
 var express = require('express');
 var request = require('request');
 require('shelljs/global');
@@ -9,8 +12,55 @@ app.use(bodyParser());
 
 var port = 3000;
 
-app.get('/build', function (req, res) {
-  res.send('Hello World!');
+
+
+// Run an Xcode sandbox
+app.post('/build-sandbox', function (req, res) {
+  	console.log('Following sandbox executed at '+ new Date());
+  	console.log(req.body.code);
+
+	// console.log((req.body.code).length);
+
+  if (req.body.code && (req.body.code).length != 0) {
+  	fs.writeFile("code.swift", req.body.code, function(err) {
+	    if(err) {
+	        return console.log(err);
+	    }
+
+	   res.send(exec("swift code.swift").output); 
+
+	    // console.log("The file was saved!");
+	}); 
+
+  } else {
+  	res.send("Nothing to compile.");
+  }
+
+});
+
+
+// Build an Xcode Project
+app.post('/build-project', function (req, res) {
+  	console.log('Following sandbox executed at '+ new Date());
+  	console.log(req.body.code);
+
+	// console.log((req.body.code).length);
+
+  if (req.body.code && (req.body.code).length != 0) {
+  	fs.writeFile("code.swift", req.body.code, function(err) {
+	    if(err) {
+	        return console.log(err);
+	    }
+
+	   res.send(exec("swift code.swift").output); 
+
+	    // console.log("The file was saved!");
+	}); 
+
+  } else {
+  	res.send("Nothing to compile.");
+  }
+
 });
 
 
