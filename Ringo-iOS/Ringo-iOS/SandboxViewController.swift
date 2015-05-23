0e1e1313c9309009e9eb2f7b179e5333552e80b1
gmittal <gautam@mittal.net>
2015-05-23 14:54:56 -0700

    got nav controller working

---
+++ Ringo-iOS/Ringo-iOS/SandboxViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SandboxViewController.swift
+//  Ringo-iOS
+//
+//  Created by Gautam Mittal on 5/23/15.
+//  Copyright (c) 2015 Gautam Mittal. All rights reserved.
+//
+
+import UIKit
+
+class SandboxViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
