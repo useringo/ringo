60202f127d4db0380a2cbb681c75a5dcc8e705d5
gmittal <gautam@mittal.net>
2015-07-11 14:03:10 -0700

    more fixes

--- Ringo-Web/project/js/main.js
+++ Ringo-Web/project/js/main.js
@@ -3,7 +3,7 @@ var typingTimer;                //timer identifier
 var doneTypingInterval = 1000;  //time in ms, 5 second for example
 
 
-var hostname = "https://3f6614c4.ngrok.com";
+var hostname = "https://78a9b4ae.ngrok.com";
 
 var project_id = ""; //prompt("Type your Ringo Project ID");
 var project_name = "";
