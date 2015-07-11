f88f136cb5aef2eb8ed4486e17c35b39d771df46
gmittal <gautam@mittal.net>
2015-07-11 13:32:01 -0700

    eliminated need for firebase module

--- build-server/app.js
+++ build-server/app.js
@@ -5,7 +5,7 @@
   Dependencies:
   NODE.JS + Xcode 6.3 + Homebrew + Ruby 2.2.2
 
-  $ sudo npm install && gem install nomad-cli && brew install wget
+  $ sudo npm install && gem install nomad-cli && brew install wget zip unzip
 
   You will also need to populate the .env file with the necessary environment variables in order for this script to run effectively
 
@@ -23,7 +23,6 @@ dotenv.load();
 var async = require('async');
 var bodyParser = require('body-parser');
 var colors = require('colors');
-var Firebase = require('firebase');
 var fs = require('fs');
 var express = require('express');
 var request = require('request');
@@ -66,9 +65,6 @@ ngrok.connect(port, function (err, url) {
 
 
 });
-
-
-var uid_maker = new Firebase(process.env.FIREBASE); // utilizing Firebase to generate unique keys :P
 
 
 app.get('/get-secure-tunnel', function (req, res) {
@@ -167,7 +163,7 @@ app.post('/create-project', function(req, res) {
   // only execute if they specify the required parameters
   if (req.body.projectName) {
         var projectName = req.body.projectName;
-        var project_uid = uid_maker.push().key();
+        var project_uid = generatePushID();
         project_uid = project_uid.substr(1, project_uid.length);
 
         console.log(project_uid);
@@ -284,7 +280,7 @@ app.post('/upload-project-zip', function (req, res) {
     cd(buildProjects_path);
 
     // create a unique ID where this awesome project will live
-    var project_uid = uid_maker.push().key();
+    var project_uid = generatePushID();
     project_uid = project_uid.substr(1, project_uid.length);
 
     console.log(project_uid);
@@ -338,7 +334,7 @@ app.post('/clone-git-project', function (req, res) {
     console.log('Received request to git clone a file.');
 
     // create a unique ID where this awesome project will live
-    var project_uid = uid_maker.push().key();
+    var project_uid = generatePushID();
     project_uid = project_uid.substr(1, project_uid.length);
 
     console.log(project_uid);
@@ -895,4 +891,62 @@ Array.prototype.remove = function() {
     return this;
 };
 
+
+/**
+ * Fancy ID generator that creates 20-character string identifiers with the following properties:
+ *
+ * 1. They're based on timestamp so that they sort *after* any existing ids.
+ * 2. They contain 72-bits of random data after the timestamp so that IDs won't collide with other clients' IDs.
+ * 3. They sort *lexicographically* (so the timestamp is converted to characters that will sort properly).
+ * 4. They're monotonically increasing.  Even if you generate more than one in the same timestamp, the
+ *    latter ones will sort after the former ones.  We do this by using the previous random bits
+ *    but "incrementing" them by 1 (only in the case of a timestamp collision).
+ */
+generatePushID = (function() {
+  // Modeled after base64 web-safe chars, but ordered by ASCII.
+  var PUSH_CHARS = '-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz';
+ 
+  // Timestamp of last push, used to prevent local collisions if you push twice in one ms.
+  var lastPushTime = 0;
+ 
+  // We generate 72-bits of randomness which get turned into 12 characters and appended to the
+  // timestamp to prevent collisions with other clients.  We store the last characters we
+  // generated because in the event of a collision, we'll use those same characters except
+  // "incremented" by one.
+  var lastRandChars = [];
+ 
+  return function() {
+    var now = new Date().getTime();
+    var duplicateTime = (now === lastPushTime);
+    lastPushTime = now;
+ 
+    var timeStampChars = new Array(8);
+    for (var i = 7; i >= 0; i--) {
+      timeStampChars[i] = PUSH_CHARS.charAt(now % 64);
+      // NOTE: Can't use << here because javascript will convert to int and lose the upper bits.
+      now = Math.floor(now / 64);
+    }
+    if (now !== 0) throw new Error('We should have converted the entire timestamp.');
+ 
+    var id = timeStampChars.join('');
+ 
+    if (!duplicateTime) {
+      for (i = 0; i < 12; i++) {
+        lastRandChars[i] = Math.floor(Math.random() * 64);
+      }
+    } else {
+      // If the timestamp hasn't changed since last push, use the same random number, except incremented by 1.
+      for (i = 11; i >= 0 && lastRandChars[i] === 63; i--) {
+        lastRandChars[i] = 0;
+      }
+      lastRandChars[i]++;
+    }
+    for (i = 0; i < 12; i++) {
+      id += PUSH_CHARS.charAt(lastRandChars[i]);
+    }
+    if(id.length != 20) throw new Error('Length should be 20.');
+ 
+    return id;
+  };
+})();
 
