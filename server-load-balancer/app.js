5dc809bb94192b11baa794cdba204cbdcaf62994
gmittal <gautam@mittal.net>
2015-07-29 14:52:15 -0700

    very very basic, not super amazing or fancy load balancer created

--- server-load-balancer/app.js
+++ server-load-balancer/app.js
@@ -8,34 +8,52 @@ app.use(bodyParser());
 
 
 var servers = {};
+var ids = [];
 var currentServer = 0;
 
 // We only have two servers at the moment
 
 // Endpoint to serve the user so that it 
 app.get('/get-server-url', function (req, res) {
-  if (currentServer == 0) {
+	var numRegisteredServers = 0;
+	for (var id in servers) {
+		numRegisteredServers++;
+	}
 
-  	res.send();
-  }
+	console.log(numRegisteredServers);
 
-  
+
+	res.send(servers[ids[currentServer]]);
+
+	currentServer++;
+  	if (currentServer > numRegisteredServers) {
+  		currentServer = 0;
+  	}
+
 });
 
 
+// Register the server with the load balancer
 app.post('/register-server', function (req, res) {
 	if (req.body.tunnel) {
 		if (req.body.server_id) {
-			// var server_data = {};
-			// server_data[req.body.server_id] = req.body.tunnel;
 			servers[req.body.server_id] = req.body.tunnel;
 
-			console.log(servers);
-			res.send("Successfully registered server in the load balancer.");
+			if (ids.indexOf(req.body.server_id) == -1) { // basically, if it doesn't already exist
+				ids.push(req.body.server_id);
+			}
+			
 
+			console.log(servers);
+			console.log(ids);
+			res.send("Successfully registered server in the load balancer.");
 		
+		} else {
+			res.send(500, "There was an error registering the server with the load balancer.");
 		}
 		
+	} else {
+		res.send(500, "There was an error registering the server with the load balancer.");
 	}
 });
 
